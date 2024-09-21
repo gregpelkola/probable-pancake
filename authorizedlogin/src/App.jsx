324e3bcc648b349ff7e1ec553f781ee05f1fa235
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-09-21 09:33:27 -0500

    created basic login form

--- authorizedlogin/src/App.jsx
+++ authorizedlogin/src/App.jsx
@@ -16,5 +16,30 @@ function App() {
     }
   };
 
-  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>Username:</label>
+            <input 
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input 
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <p>{message}</p>
+      </header>
+    </div>
+  );
 }
+
+export default App;
